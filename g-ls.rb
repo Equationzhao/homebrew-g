b4f171fd3aaca219fbe3b31739a2fba44df61b49
24601 <equationzhao@foxmail.com>
2023-08-10 01:50:10 +0800

    reversion

--- g-ls.rb
+++ g-ls.rb
@@ -1,9 +1,10 @@
 class GLs < Formula
   desc "a powerfull cross-platform ls"
   homepage "g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.11.5.tar.gz" , :tag => "v0.11.5"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.11.5.tar.gz" , :tag => "v0.11.5-2"
   sha256 "0488c5804f1c4d2662329656e1bc703f0003c374025f5af63ddb252241993c7a"
   license "MIT"
+  reversion 2
 
   depends_on "go" => :build
 
