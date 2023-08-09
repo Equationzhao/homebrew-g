1e9b88bf00660b1eddd9fb57b9459168a5605b43
24601 <equationzhao@foxmail.com>
2023-08-10 01:39:37 +0800

    fix tag

--- g-ls.rb
+++ g-ls.rb
@@ -1,7 +1,7 @@
 class GLs < Formula
   desc "a powerfull cross-platform ls"
   homepage "g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.11.5.tar.gz" :tag => "v0.11.5"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.11.5.tar.gz" , :tag => "v0.11.5"
   sha256 "0488c5804f1c4d2662329656e1bc703f0003c374025f5af63ddb252241993c7a"
   license "MIT"
 
