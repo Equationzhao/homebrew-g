75cc176069abf66ceb8052f36dbd3ef3bcd1958a
24601 <equationzhao@foxmail.com>
2023-09-11 18:57:49 +0800

    new patch

--- g-ls.rb
+++ g-ls.rb
@@ -1,8 +1,8 @@
 class GLs < Formula
   desc "a powerfull cross-platform ls"
   homepage "g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.14.0.tar.gz" , :tag => "v0.14.0"
-  sha256 "3bbe10a755005e6e2e9ad64a54035dff8014b639a39623d0a7503fa690aa4b23"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.14.1.tar.gz" , :tag => "v0.14.1"
+  sha256 "432478b7b7770e4cb05174a5be544827d88bda9e1c0d6b673952d30b9489db6e"
   license "MIT"
 
   depends_on "go" => :build
