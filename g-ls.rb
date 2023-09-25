632f451475adf085a6b84c677e34cce7e47f0c17
24601 <equationzhao@foxmail.com>
2023-09-25 18:22:48 +0800

    chore: :rotating_light: pass brew test

--- g-ls.rb
+++ g-ls.rb
@@ -1,7 +1,7 @@
 class GLs < Formula
-  desc "a powerfull cross-platform ls"
-  homepage "g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.16.0.tar.gz" , :tag => "v0.16.0"
+  desc "Powerfull cross-platform ls"
+  homepage "https://g.equationzhao.space"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.16.0.tar.gz", tag: "v0.16.0"
   sha256 "d67812907da591aed132ec0190461a007f9962284ca2c40e66bb123a59041445"
   license "MIT"
 
@@ -10,5 +10,10 @@ class GLs < Formula
   def install
     system "go build -ldflags='-s -w'"
     bin.install "g"
+  end
+
+  test do
+    output = shell_output("#{bin}/g -v | grep -E 'Version\\s*[0-9.]*' | grep -v 'Go Version' | awk '{print $3}'")
+    assert_match "0.16.0", output
   end
 end
