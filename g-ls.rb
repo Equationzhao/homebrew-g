887a3037c3674a7f6475769f1e8bcadfe29e1e15
24601 <equationzhao@foxmail.com>
2023-10-22 00:44:51 +0800

    new minor version

--- g-ls.rb
+++ g-ls.rb
@@ -1,8 +1,8 @@
 class GLs < Formula
   desc "Powerfull cross-platform ls"
   homepage "https://g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.21.2.tar.gz", tag: "v0.21.2"
-  sha256 "fd13c7c46a002146e61a8326645f260ff80d9078b7bc896ea8603c4b10d0b562"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.22.0.tar.gz", tag: "v0.22.0"
+  sha256 "b3d42afe6872d3783c3545acb2c10a8033426b7f9a71698564ada69ca4977677"
   license "MIT"
 
   depends_on "go" => :build
@@ -14,6 +14,6 @@ class GLs < Formula
 
   test do
     output = shell_output("#{bin}/g -v | grep -E 'Version\\s*[0-9.]*' | grep -v 'Go Version' | awk '{print $3}'")
-    assert_match "0.21.2", output
+    assert_match "0.22.0", output
   end
 end
